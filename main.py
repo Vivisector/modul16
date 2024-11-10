5e68fcfae69d446525d90c75ee5a58beb7e50c9e
Dmitry <hulk295@yandex.ru>
2024-11-10 19:14:49 +0300

    Основы Fast Api и маршрутизация (а также документация в Swagger [добавляем через слеш после порта 'docs']))

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+# async def welcome() -> dict:
+async def welcome():
+    # return {'message': "Главная страница"}
+    return "Главная страница"
+
+@app.get("/user/admin/")
+# async def news(first_name: str, last_name: str) -> dict:
+# можно и без аннотации ->dict.
+async def admin():
+# async def admin() -> dict:
+    return 'Вы вошли как администратор'
+    # return {"message": 'Вы вошли как администратор'}
+
+@app.get('/user/{user_id}')
+async def user_id(user_id: int):
+    return f'Вы вошли как пользователь № {user_id}'
+
+@app.get('/user/')
+async def user_id(user_name: str, age: int):
+    return f'Информация о пользователе. Имя: {user_name}. Возраст {age}'
